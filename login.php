46a97cb086808a7b739c952a9c587f3458ac09ec
Stuart Muyambi <stuartmuyambi@gmail.com>
2019-12-15 00:59:29 +0300

    Finish Login and registration pages

--- login.php
+++ login.php
@@ -10,15 +10,65 @@
     <title>Login | Campus Ballot</title>
 </head>
 <body class="login-body">
-    <div class="form-container">
+    <div class="form-container shadow">
         <div class="form-section dark-form-section">
             <i class="fas fa-vote-yea vote-icon"></i>
             <h1 class="slogan" id="slogan">Your <span class="vote">vote</span> your <span class="line">voice.</span></h1>
         </div>
 
-        <div class="form-section white-form-section"></div>
+        <div class="form-section white-form-section">
+            <div class="input-container">
+                <h2 class="text-logo"><i class="fas fa-vote-yea"></i> Campus Ballot</h2>
+                <p class="lead">We're happy to see you again!</p>
+
+                <form action="" method="post" class="mx-auto">
+                    <!-- <div class="errors bummer">
+                        <div class="icon"><i class="fas fa-times-circle"></i></div>
+                        <div class="error-message">
+                            <strong>Don't:</strong> Waste precious above the fold space with vague calls calls to action like this
+                        </div>
+                    </div> -->
+
+                    <!-- <div class="errors hooray">
+                        <div class="icon"><i class="fas fa-check-circle"></i></div>
+                        <div class="error-message">
+                            <strong>Don't:</strong> Waste precious above the fold space with vague calls calls to action like this
+                        </div>
+                    </div> -->
+
+                    <!-- <div class="errors check">
+                        <div class="icon"><i class="fas fa-exclamation-triangle"></i></div>
+                        <div class="error-message">
+                            <strong>Don't:</strong> Waste precious above the fold space with vague calls calls to action like this
+                        </div>
+                    </div> -->
+
+                    <div class="input-group mt-3 pt-2 mb-3">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fas fa-user"></i></div>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Username">
+                    </div>
+
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fas fa-key"></i></div>
+                        </div>
+                        <input type="password" class="form-control" placeholder="Password">
+                    </div>
+
+                    <div class="custom-control custom-checkbox mr-sm-2 mt-4">
+                        <input type="checkbox" class="custom-control-input" id="customControlAutosizing">
+                        <label class="custom-control-label" for="customControlAutosizing" style="color: #6c757d;">Remember me</label>
+                    </div>
+
+                    <div class="pt-2">
+                        <button type="submit" class="btn btn-primary btn-block mt-5">Login</button>
+                    </div>
+                    </form>
+            </div>
+        </div>
     </div>
 
-    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
--- register.php
+++ register.php
@@ -9,9 +9,82 @@
     <link rel="stylesheet" href="css/all.min.css">
     <title>Register | Campus Ballot</title>
 </head>
-<body>
-    <
+<body class="login-body">
+    <div class="form-container shadow">
+        <div class="form-section dark-form-section">
+            <i class="fas fa-vote-yea vote-icon"></i>
+            <h1 class="slogan" id="slogan">Your <span class="vote">vote</span> your <span class="line">voice.</span></h1>
+        </div>
 
-    <script src="js/bootstrap.min.js"></script>
+        <div class="form-section white-form-section">
+            <div class="input-container">
+                <h2 class="text-logo"><i class="fas fa-vote-yea"></i> Campus Ballot</h2>
+                <p class="lead">Register Now! Let your voice be heard</p>
+
+                <form action="" method="post" class="mx-auto">
+                    <!-- <div class="errors bummer">
+                        <div class="icon"><i class="fas fa-times-circle"></i></div>
+                        <div class="error-message">
+                            <strong>Don't:</strong> Waste precious above the fold space with vague calls calls to action like this
+                        </div>
+                    </div> -->
+
+                    <!-- <div class="errors hooray">
+                        <div class="icon"><i class="fas fa-check-circle"></i></div>
+                        <div class="error-message">
+                            <strong>Don't:</strong> Waste precious above the fold space with vague calls calls to action like this
+                        </div>
+                    </div> -->
+
+                    <!-- <div class="errors check">
+                        <div class="icon"><i class="fas fa-exclamation-triangle"></i></div>
+                        <div class="error-message">
+                            <strong>Don't:</strong> Waste precious above the fold space with vague calls calls to action like this
+                        </div>
+                    </div> -->
+
+                    <div class="input-group mt-5 mb-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fas fa-user"></i></div>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Username">
+                    </div>
+
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fas fa-envelope"></i></div>
+                        </div>
+                        <input type="email" class="form-control" placeholder="Email">
+                    </div>
+
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fas fa-address-card"></i></div>
+                        </div>
+                        <input type="text" class="form-control" placeholder="ID Number">
+                    </div>
+
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fas fa-key"></i></div>
+                        </div>
+                        <input type="password" class="form-control" placeholder="Password">
+                    </div>
+
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fas fa-key"></i></div>
+                        </div>
+                        <input type="password" class="form-control" placeholder="Confirm Password">
+                    </div>
+
+                    <div>
+                        <button type="submit" class="btn btn-primary btn-block mt-4">Create Account</button>
+                    </div>
+                    </form>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
