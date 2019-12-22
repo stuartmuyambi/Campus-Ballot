98406d95e0bc2c00f1b9272fbc745ac8a847de02
Stuart Muyambi <stuartmuyambi@gmail.com>
2019-12-23 00:04:21 +0300

    Add silly accordion

--- candidates.php
+++ candidates.php
@@ -41,16 +41,26 @@
 
     <!-- Main Body  -->
     <div class="main-body">
-        <div class="voters-container mt-5 pt-5 pb-5">
-            <div class="table-container shadow">
-                <div class="ribbon">
-                    ...
+        <div class="voters-container mt-5 pt-5 pb-5 pr-5 pl-5">
+
+                <button class="accordion">Section 1</button>
+                <div class="panel">
+                    <p>Lorem ipsum...</p>
                 </div>
 
-            </div>
+                <button class="accordion">Section 2</button>
+                <div class="panel">
+                    <p>Lorem ipsum...</p>
+                </div>
+                <button class="accordion">Section 3</button>
+                <div class="panel">
+                    <p>Lorem ipsum...</p>
+                </div>
+
         </div>
     </div>
 
+    <script src="js/main.js"></script>
     <!-- End of main body  -->
 </body>
 </html>
