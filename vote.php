16bf9b617ba6a5c59464ca0171bde8d13290a996
Stuart Muyambi <stuartmuyambi@gmail.com>
2019-12-22 22:38:27 +0300

    Add table container ribbon

--- vote.php
+++ vote.php
@@ -42,7 +42,10 @@
     <!-- Main Body  -->
     <div class="main-body">
         <div class="voters-container mt-5 pt-5 pb-5">
-            <div class="table-container shadow-sm">
+            <div class="table-container shadow">
+                <div class="table-ribbon">
+                    ...
+                </div>
 
             </div>
         </div>
