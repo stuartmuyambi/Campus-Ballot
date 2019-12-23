bdccc7a8a72f9ec6cb9b5f68cdcc93a74ac6e940
Stuart Muyambi <stuartmuyambi@gmail.com>
2019-12-23 15:42:54 +0300

    Add footer and header to register.php file

--- register.php
+++ register.php
@@ -1,16 +1,9 @@
-<?php include('functions.php') ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-    <link rel="stylesheet" href="css/all.min.css">
-    <link rel="stylesheet" href="css/main.min.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <title>Register - Campus Ballot</title>
-</head>
+<?php 
+    include('functions.php') 
+?>
+
+<?php include("includes/boilerplate.php"); ?>
+
 <body class="login-body">
     <div class="form-container shadow">
         <div class="form-section dark-form-section">
@@ -69,6 +62,5 @@
             </div>
         </div>
     </div>
-
-</body>
-</html>
+    
+<?php include("includes/footer.php"); ?>
