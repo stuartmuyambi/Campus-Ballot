31b07d8a4ba7a2e94be8e3eb5bf59a0ed0709e9a
Stuart Muyambi <stuartmuyambi@gmail.com>
2019-12-23 23:02:32 +0300

    Add more nav icons

--- includes/header.php
+++ includes/header.php
@@ -19,10 +19,12 @@
 
         <div class="dash-section-2"></div>
 
-        <div class="dash-section-3">
+        <div class="dash-section-3 nav-icons">
+            <a href="#"><i class="fas fa-th fa-lg"></i></a>            
+            <a href="#"><i class="fas fa-user-circle fa-lg"></i></a>
             <?php  if(isset($_SESSION['user'])) : ?>
                 <a href="index.php?logout='1'">
-                    <i class="fas fa-power-off fa-lg" style="color: #bb1333;"> </i> <span class="logout"> Logout</span>
+                    <i class="fas fa-power-off fa-lg"> </i>
                 </a>
             <?php  endif ?>
         </div>
