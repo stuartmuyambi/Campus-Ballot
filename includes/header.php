a53d48a95bda96578ca34d3a9efe95a6dd6a02c5
Stuart Muyambi <stuartmuyambi@gmail.com>
2019-12-23 23:48:28 +0300

    Add notification icon on navigation

--- includes/header.php
+++ includes/header.php
@@ -21,6 +21,7 @@
 
         <div class="dash-section-3 nav-icons">
             <a href="index.php"><i class="fas fa-th fa-lg icon"></i></a>            
+            <a href="#"><i class="fas fa-bell fa-lg icon"></i></a>
             <a href="#"><i class="fas fa-user-circle fa-lg icon"></i></a>
             <?php  if(isset($_SESSION['user'])) : ?>
                 <a href="index.php?logout='1'">
