7391f8dec12375bb2ae8f74694e1fd3e8eea1aab
Stuart Muyambi <stuartmuyambi@gmail.com>
2019-12-29 23:03:06 +0300

    Add simple bootstrap table template for posts

--- vote.php
+++ vote.php
@@ -15,11 +15,37 @@
     <!-- Main Body  -->
     <div class="main-body">
         <div class="voters-container mt-5 pt-5 pb-5">
-            <div class="table-container shadow pb-5">
-                <div class="ribbon">
-                    ...
-                </div>
-
+            <div class="table-container shadow pt-5 pl-5 pr-5 pb-5">
+                <table class="table table-striped">
+                    <thead class="thead-dark">
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
