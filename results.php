c2d55847f981c5ea1d12e6090cefa9f041979254
Stuart Muyambi <stuartmuyambi@gmail.com>
2019-12-31 00:08:04 +0300

    Add results page

---
+++ results.php
@@ -0,0 +1,30 @@
+<?php
+    include('functions.php'); //add function.php script to page
+    $pageTitle = $_SESSION['user']['username'] . " - Campus Ballot";
+
+    //Prevent unauthorised user access to this page
+    if (!isLoggedIn()) {
+        $_SESSION['msg'] = "Please login to continue";
+        header('location: login.php');
+    }
+
+    // Connect to MySQL
+    $pdo = pdo_connect_mysql();
+
+    // MySQL query that selects all the polls and poll answers
+    $stmt = $pdo->query('SELECT p.*, GROUP_CONCAT(pa.title ORDER BY pa.id) AS answers FROM polls p LEFT JOIN poll_answers pa ON pa.poll_id = p.id GROUP BY p.id');
+    $polls = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php include("includes/header.php"); ?>
+
+    <!-- Main Body  -->
+    <div class="main-body">
+        <div class="voters-container mt-5 pt-5 pb-5">
+            <div class="table-container shadow pt-5 pl-5 pr-5 pb-5">
+                
+            </div>
+        </div>
+    </div>
+
+<?php include("includes/footer.php"); ?>
