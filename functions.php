326dbf1eb2df880990a3d10772e97994ecde935f
Stuart Muyambi <stuartmuyambi@gmail.com>
2019-12-30 22:43:58 +0300

    Add pdo function to connect to database

--- functions.php
+++ functions.php
@@ -182,4 +182,19 @@ function isAdmin() {
     }
 }
 
+// Poll connection to database
+function pdo_connect_mysql() {
+    // Update the details below with your MySQL details
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'stuartmuyambi';
+    $DATABASE_PASS = 'msj3p36n';
+    $DATABASE_NAME = 'campus_ballot';
+    try {
+    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+    	// If there is an error with the connection, stop the script and display the error.
+    	die ('Failed to connect to database!');
+    }
+}
+
 ?>
