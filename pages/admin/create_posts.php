c30fd8774987229f591cba00dda472ed9793994e
Stuart Muyambi <stuartmuyambi@gmail.com>
2019-12-31 09:29:06 +0300

    Add admin page to add new posts for aspiring candidates

---
+++ pages/admin/create_posts.php
@@ -0,0 +1,23 @@
+<?php
+    include('functions.php'); //add function.php script to page
+    $pageTitle = $_SESSION['user']['username'] . " - Campus Ballot";
+
+    //Prevent unauthorised user access to this page
+    if (!isLoggedIn()) {
+        $_SESSION['msg'] = "Please login to continue";
+        header('location: login.php');
+    }
+?>
+
+<?php include("includes/header.php"); ?>
+
+    <!-- Main Body  -->
+    <div class="main-body">
+        <div class="voters-container mt-5 pt-5 pb-5">
+            <div class="table-container shadow pt-5 pl-5 pr-5 pb-5">
+                <h1>Hello World</h1>
+            </div>
+        </div>
+    </div>
+
+<?php include("includes/footer.php"); ?>
