f9d88347fb338f9650bc46c6ba59ef229d14b484
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-02 14:35:26 +0300

    Remove stupid poll table and its php code

--- vote.php
+++ vote.php
@@ -7,13 +7,6 @@
         $_SESSION['msg'] = "Please login to continue";
         header('location: login.php');
     }
-
-    // Connect to MySQL
-    $pdo = pdo_connect_mysql();
-    
-    // MySQL query that selects all the polls and poll answers
-    $stmt = $pdo->query('SELECT p.*, GROUP_CONCAT(pa.title ORDER BY pa.id) AS answers FROM polls p LEFT JOIN poll_answers pa ON pa.poll_id = p.id GROUP BY p.id');
-    $polls = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <?php include("includes/header.php"); ?>
@@ -29,30 +22,7 @@
                     </div>
                     <input type="text" class="form-control" id="myInput" onkeyup="filterTable()" placeholder="Search">
                 </div>
-
-                <table class="table table-striped" id="myTable">
-                    <thead class="thead-dark">
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Title</th>
-                        <th scope="col">Answers</th>
-                        <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($polls as $poll): ?>
-                        <tr>
-                            <td><?=$poll['id']?></td>
-                            <td><?=$poll['title']?></td>
-                            <td><?=$poll['answers']?></td>
-                            <td class="a#ns">
-                                <a href="vote.php?id=<?=$poll['id']?>" class="btn btn-success btn-sm" title="View Poll"><i class="fas fa-eye fa-xs"></i></a>
-                                <a href="delete.php?id=<?=$poll['id']?>" class="btn btn-danger btn-sm" title="Delete Poll"><i class="fas fa-trash fa-xs"></i></a>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+                
             </div>
         </div>
     </div>
