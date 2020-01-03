5a1cb53e3a3205e878d6e904f3f98de1fb82700f
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-03 21:08:59 +0300

    Change home icon

--- includes/header.php
+++ includes/header.php
@@ -20,8 +20,8 @@
         <div class="dash-section-2"></div>
 
         <div class="dash-section-3 nav-icons">
-            <a href="index.php"><i class="fas fa-th fa-lg icon"></i></a>            
-            <a href="#"><i class="fas fa-bell fa-lg icon"></i></a>
+            <a href="index.php"><i class="fas fa-home fa-lg icon"></i></a>
+            <a href="#"><i class="fas fa-th fa-lg icon"></i></a>            
             <a href="#"><i class="fas fa-user-circle fa-lg icon"></i></a>
             <?php  if(isset($_SESSION['user'])) : ?>
                 <a href="index.php?logout='1'">
