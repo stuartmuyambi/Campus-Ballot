6319ff6f0a56b53ad065ccce2addf1f588491723
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-07 23:37:15 +0300

    Add app database

---
+++ database/campus_ballot.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 07, 2020 at 08:35 PM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `campus_ballot`
+--
+CREATE DATABASE IF NOT EXISTS `campus_ballot` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `campus_ballot`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `user_type` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `user_type`, `password`) VALUES
+(1, 'Stuart Muyambi', 'stuartmuyambi@gmail.com', 'admin', 'f01fa46fd13db60092dde5ccb386f153'),
+(8, 'Brad Traversy', 'techguy@gmail.com', 'user', 'f01fa46fd13db60092dde5ccb386f153'),
+(9, 'Stuart', 'stuartmuyambi@gmail.com', 'user', 'f01fa46fd13db60092dde5ccb386f153');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
