eb9a4949ee664f797da85450dabc60553c3c55cc
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-09 02:30:24 +0300

    Remove accordion js code

--- js/main.js
+++ js/main.js
@@ -9,16 +9,3 @@ function togglePassword() {
     }
     }
 
-// Accordion
-let acc = document.getElementsByClassName("accordion");
-let i;
-for (i = 0; i < acc.length; i++) {  
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        let panel = this.nextElementSibling;
-        if (panel.style.maxHeight){panel.style.maxHeight = null;
-        } else {      
-            panel.style.maxHeight = panel.scrollHeight + "px";    
-        }   });
-    }
-
