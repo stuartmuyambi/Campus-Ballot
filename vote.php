e26735c67db8dea9526463681034899006bb2e08
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-09 00:31:13 +0300

    Remove error on line  16

--- vote.php
+++ vote.php
@@ -8,12 +8,6 @@
         header('location: login.php');
     }
 
-    // Connect to MySQL
-    $pdo = pdo_connect_mysql();
-
-    // MySQL query that selects all the polls and poll answers
-    $stmt = $pdo->query('SELECT p.*, GROUP_CONCAT(pa.title ORDER BY pa.id) AS answers FROM polls p LEFT JOIN poll_answers pa ON pa.poll_id = p.id GROUP BY p.id');
-    $polls = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <?php include("includes/header.php"); ?>
