76f29adc50c0dba2fc68d919fbe904aac433b24f
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-12 20:48:51 +0300

    add horizontal rule to devide voting cards

--- vote.php
+++ vote.php
@@ -17,7 +17,8 @@
         <section class="mt-2 pt-5 pb-5">
             <div class="thumbs-container shadow pt-4 pl-5 pr-5 pb-5">
                 <p><a href="index.php" class="home-pagination">Home</a></p>
-                <h3 class="voting-heading">Available Candidates</h3>
+                <h3 class="voting-heading pb-3">Available Candidates</h3>
+                <hr>
                
             </div>
         </section>
