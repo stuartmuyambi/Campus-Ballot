3826094a457f9bb05cb1d8fc107d95912cf3bcd4
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-12 20:42:35 +0300

    Edit voters page pagination

--- vote.php
+++ vote.php
@@ -16,7 +16,7 @@
     <div class="main-body">
         <section class="mt-2 pt-5 pb-5">
             <div class="thumbs-container shadow pt-4 pl-5 pr-5 pb-5">
-                <p><a href="index.php">Home</a></p>
+                <p><a href="index.php" class="home-pagination">Home</a></p>
                 <h3 class="voting-heading">Available Candidates</h3>
                
             </div>
