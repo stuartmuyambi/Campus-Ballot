974b4a0db5b2851aa450751b99c685c921a0892f
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-13 22:32:39 +0300

    Add flexbox to voting radio buttons

--- vote.php
+++ vote.php
@@ -16,26 +16,30 @@
     <div class="main-body">
         <section class="thumbs-container shadow mt-5 pt-4 pl-5 pr-5 pb-5">
             <p><a href="index.php" class="home-pagination">Home</a></p>
-            <h3 class="voting-heading pb-3">Candidates</h3>
+            <h3 class="voting-heading pb-3">Aspiring Candidates</h3>
             <hr class="pb-4">
 
             <section class="candidate-thumbnail-container">
                 <div class="candidate-thumbnail">
                     <h3 class="thumbnail-heading">Guild President  <span class="badge badge-primary">4</span></h3>
 
-                    <div class="select shadow-sm mb-2 mt-3">
-                        <div class="custom-control custom-radio ml-2">
-                            <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input ml-3">
-                            <label class="custom-control-label" for="customRadio1">Lemi Simon</label>
+                    <section class="select-container">
+                        <div class="select shadow-sm mb-2 mt-3">
+                            <div class="custom-control custom-radio ml-2">
+                                <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input ml-3">
+                                <label class="custom-control-label" for="customRadio1">Lemi Simon</label>
+                            </div>
                         </div>
-                    </div>
-
-                    <div class="select shadow-sm">
-                        <div class="custom-control custom-radio ml-2">
-                            <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input ml-3">
-                            <label class="custom-control-label" for="customRadio2">Rukundo Irene</label>
+    
+                        <div class="select shadow-sm">
+                            <div class="custom-control custom-radio ml-2">
+                                <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input ml-3">
+                                <label class="custom-control-label" for="customRadio2">Rukundo Irene</label>
+                            </div>
                         </div>
-                    </div>
+                        
+                        <button type="button" class="btn btn-primary btn-sm btn-block">VOTE</button>
+                    </section>
 
                 </div>
                 <div class="candidate-thumbnail">...</div>
