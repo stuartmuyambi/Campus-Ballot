e4aa48bbee37a343447d2475b5a92539602f4dd5
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-13 16:24:50 +0300

    Remove accordion styles

--- vote.php
+++ vote.php
@@ -17,8 +17,20 @@
         <section class="mt-2 pt-5 pb-5">
             <div class="thumbs-container shadow pt-4 pl-5 pr-5 pb-5">
                 <p><a href="index.php" class="home-pagination">Home</a></p>
-                <h3 class="voting-heading pb-3">Available Candidates</h3>
-                <hr>
+                <h3 class="voting-heading pb-3">Candidates</h3>
+                <hr class="pb-4">
+
+                <div class="candidate-thumbnail">
+                    ...
+                </div>
+
+                <div class="candidate-thumbnail">
+                    ...
+                </div>
+                
+                <div class="candidate-thumbnail">
+                    ...
+                </div>
                
             </div>
         </section>
