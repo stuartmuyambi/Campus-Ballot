326b28e65fbbf7f5b1eda8ef0c37b7c890c81287
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-14 14:24:59 +0300

    Add semantics to the index page

--- index.php
+++ index.php
@@ -7,53 +7,57 @@
         $_SESSION['msg'] = "Please login to continue";
         header('location: login.php');
     }
+
 ?>
 
 <?php include("includes/header.php")?>
 
     <!-- Main Body  -->
-    <div class="main-body">
+    <main class="main-body">
 
-    <?php  if(isset($_SESSION['user'])) : ?>
-        <h1 class="display-4 mt-3">Hello <?php echo $_SESSION['user']['username'];?>
-    <?php  endif ?>
+        <?php  if(isset($_SESSION['user'])) : ?>
+            <h1 class="display-4 mt-3">Hello <?php echo $_SESSION['user']['username'];?>
+        <?php  endif ?>
 
-    <p class="index-slogan mt-2 mb-4">We're here to securely assist you in a fast and easy way possible. What would you like to achieve today:</p>
+        <p class="index-slogan mt-2 mb-4">We're here to securely assist you in a fast and easy way possible. What would you like to achieve today:</p>
 
-    <div class="card-section mt-3">
+        <section class="card-section mt-3">
 
-        <a href="vote.php">
-            <div class="card-container card-1 shrink">
-                <header class="card-header">
-                    <i class="fas fa-vote-yea fa-3x mt-4"></i>
-                </header>
-                <footer class="card-footer">
-                    <p> i want to cast my vote</p>
-                </footer>
-            </div>
-        </a>
+            <a href="vote.php">
+                <div class="card-container card-1 shrink">
+                    <header class="card-header">
+                        <i class="fas fa-vote-yea fa-3x mt-4"></i>
+                    </header>
+                    <footer class="card-footer">
+                        <p> i want to cast my vote</p>
+                    </footer>
+                </div>
+            </a>
 
-        <a href="candidates.php">
-            <div class="card-container card-2 shrink">
-                <header class="card-header">
-                    <i class="fas fa-user-friends fa-3x mt-4"></i>
-                </header>
-                <footer class="card-footer">
-                    <p>i want to view candidates</p>
-                </footer>
-            </div>
-        </a>
+            <a href="candidates.php">
+                <div class="card-container card-2 shrink">
+                    <header class="card-header">
+                        <i class="fas fa-user-friends fa-3x mt-4"></i>
+                    </header>
+                    <footer class="card-footer">
+                        <p>i want to view candidates</p>
+                    </footer>
+                </div>
+            </a>
 
-        <a href="#">
-            <div class="card-container card-3 shrink">
-                <header class="card-header">
-                    <i class="fas fa-chart-pie fa-3x mt-4"></i>
-                </header>
-                <footer class="card-footer">
-                    <p>i want to check poll results</p>
-                </footer>
-            </div>
-        </a>
-    </div>
+            <a href="#">
+                <div class="card-container card-3 shrink">
+                    <header class="card-header">
+                        <i class="fas fa-chart-pie fa-3x mt-4"></i>
+                    </header>
+                    <footer class="card-footer">
+                        <p>i want to check poll results</p>
+                    </footer>
+                </div>
+            </a>
+
+        </section>
+
+    </main>
 
 <?php include("includes/footer.php")?>
