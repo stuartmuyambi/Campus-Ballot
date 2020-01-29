6509785850b6911f7b80ee2758329b7e3403b983
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-29 10:13:09 +0300

    change voting heading tagline

--- vote.php
+++ vote.php
@@ -16,7 +16,7 @@
     <div class="main-body">
         <section class="thumbs-container shadow mt-5 pt-4 pl-5 pr-5 pb-5">
             <p><a href="index.php" class="home-pagination">Home</a></p>
-            <h3 class="voting-heading pb-3">Aspiring Candidates</h3>
+            <h3 class="voting-heading pb-3">Guild Nominations</h3>
             <hr class="pb-4">
 
             <section class="vote-section shadow-sm pt-4">
