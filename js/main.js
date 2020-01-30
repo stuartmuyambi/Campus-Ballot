f67e342de93b72fb7a4a51e6f8bf241b024bce43
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-30 15:39:21 +0300

    Refactor table filter function

--- js/main.js
+++ js/main.js
@@ -9,8 +9,10 @@ function togglePassword() {
     }
     }
 
+
 // Filter table content on the voting page
 function myFunction() {
+    
     // Declare variables 
     let input, filter, table, tr, td, i;  
     input = document.getElementById("myInput");
@@ -20,14 +22,14 @@ function myFunction() {
 
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
-            if (td) {
-                if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                      } else {
-                          tr[i].style.display = "none";      
-                        }
-                    }   
-                }
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";      
             }
+        }   
+    }
+}
 
