1bf14648f74baf9e18a2d344cf005fde1b6c5309
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-30 15:05:43 +0300

    Add bootstrap table template for voting records

--- vote.php
+++ vote.php
@@ -18,7 +18,36 @@
     <p class="index-slogan mt-2 mb-4">We're excited you've decided to cast your vote today. Let's help you get started.</p>
 
         <section class="thumbs-container shadow mt-5 pt-4 pl-5 pr-5 pb-5">
-            
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+            </table>
             
         </section>
         
