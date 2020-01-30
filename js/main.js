c56338ba22770aeb385cea78fa0c4c9b0b97727a
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-01-30 15:47:40 +0300

    Change the seach index of the table

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function togglePassword() {
 
 // Filter table content on the voting page
 function myFunction() {
-    
+
     // Declare variables 
     let input, filter, table, tr, td, i;  
     input = document.getElementById("myInput");
@@ -22,7 +22,11 @@ function myFunction() {
 
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
+        td = tr[i].getElementsByTagName("td")[1];
+
+        // Tip: Remove toUpperCase() if you want to perform a case-sensitive search.
+        // Tip: Change tr[i].getElementsByTagName('td')[0] to [1] if you want to search for "Country" (index 1) insteadof "Name" (index 0).
+
         if (td) {
             if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
