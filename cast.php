47ccdf7ae81d1bd1b91a27c4c8da48b6826118bf
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-02-03 11:19:25 +0300

    Refactor voting source code

--- cast.php
+++ cast.php
@@ -9,26 +9,35 @@
     }
 
     // Connect to MySQL
-$pdo = pdo_connect_mysql();
-// If the GET request "id" exists (poll id)...
-if (isset($_GET['id'])) {
+    $pdo = pdo_connect_mysql();
+    
+    // If the GET request "id" exists (poll id)...
+    if (isset($_GET['id'])) {
+
     // MySQL query that selects the poll records by the GET request "id"
     $stmt = $pdo->prepare('SELECT * FROM polls WHERE id = ?');
     $stmt->execute([$_GET['id']]);
+
     // Fetch the record
     $poll = $stmt->fetch(PDO::FETCH_ASSOC);
+
     // Check if the poll record exists with the id specified
     if ($poll) {
+
         // MySQL query that selects all the poll answers
         $stmt = $pdo->prepare('SELECT * FROM poll_answers WHERE poll_id = ?');
         $stmt->execute([$_GET['id']]);
+
         // Fetch all the poll anwsers
         $poll_answers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
         // If the user clicked the "Vote" button...
         if (isset($_POST['poll_answer'])) {
+
             // Update and increase the vote for the answer the user voted for
             $stmt = $pdo->prepare('UPDATE poll_answers SET votes = votes + 1 WHERE id = ?');
             $stmt->execute([$_POST['poll_answer']]);
+            
             // Redirect user to the result page
             header ('Location: result.php?id=' . $_GET['id']);
             exit;
