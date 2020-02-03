e5e9afd84499a0899ba7baaefbf104c6cc8cd074
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-02-03 11:22:15 +0300

    Debug functions.php page

--- functions.php
+++ functions.php
@@ -191,6 +191,7 @@ function pdo_connect_mysql() {
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
+        
     	// If there is an error with the connection, stop the script and display the error.
     	die ('Failed to connect to database!');
     }
